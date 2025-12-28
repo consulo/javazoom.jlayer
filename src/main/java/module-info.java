b90cfbda7794bfc2b3c5342fc48975e8a2f302bf
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-12-28 12:35:42 +0200

    drop applet impl (due applet api removed)
    migrate to dev

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,10 @@
  * @since 16/12/2020
  */
 module javazoom.jlayer {
-	requires transitive java.desktop;
+    requires transitive java.desktop;
 
-	exports javazoom.jl.converter;
-	exports javazoom.jl.decoder;
-	exports javazoom.jl.player;
-	exports javazoom.jl.player.advanced;
+    exports javazoom.jl.converter;
+    exports javazoom.jl.decoder;
+    exports javazoom.jl.player;
+    exports javazoom.jl.player.advanced;
 }
